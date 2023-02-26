9f8f577038ee215005618151bb0901b80caee4f2
Luiz Felipe <luizfelipe0527@gmail.com>
2023-02-26 19:54:22 -0300

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+
+import { FaUsers } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <h1>Community</h1>
+    </div>
+  )
+}
+
+export default App
