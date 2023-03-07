e4bbfdbf27deeaf5dfbfa0471e808df6e376120b
Luiz Felipe <luizfelipe0527@gmail.com>
2023-03-06 23:24:01 -0300

    User register implemented

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,33 +1,96 @@
 // CSS
 import styles from "./Form.module.css";
 
+// Components
+import FlashMessage from "../../components/FlashMessage";
+
 // Icons
 import { FaEnvelope, FaLock, FaEye, FaEyeSlash, FaUser } from "react-icons/fa";
 
-import { useState, FormEvent } from "react";
+import { useState, useEffect, useRef, FormEvent } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+
+// Utils
+import { extractFormMessages } from "../../utils/extractFormMessages";
+
+// Reducers types
+import { AppDispatch, RootState } from "../../config/store";
+
+import { reset, register } from "../../slices/authSlice";
 
 const Register = () => {
     const [email, setEmail] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
 
     const [isPassVisible, setIsPassVisible] = useState<boolean>(false);
     const [isConfirmPassVisible, setIsConfirmPassVisible] = useState<boolean>(false);
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const nameInputRef = useRef<HTMLInputElement>(null!);
+
+    const dispatch = useDispatch<AppDispatch>();
+    const { loading, error, payload, success } = useSelector((state: RootState) => state.auth);
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const register = {
+        const user = {
             email,
             name,
             password,
             confirmPassword
         }
 
-        console.log(register);
+        await dispatch(register(user));
     }
+
+    // Reset all auth states
+    useEffect(() => {
+        dispatch(reset());
+    }, [dispatch]);
+
+    // Clean all inputs after user is successfully created
+    useEffect(() => {
+        if(success) {
+            setName("");
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
+
+            nameInputRef.current.focus();
+        }
+    }, [success]);
+
+   // Extract form validation messages and set to message state
+    useEffect(() => {
+        if(payload) {
+            if(typeof payload.message === "string") {
+                setMessage(payload.message);
+            } else {
+                const extractedMessages = extractFormMessages(payload);
+                
+                if(extractedMessages) {
+                    const isEnglishMessage : boolean = extractedMessages[0].includes("must be");
+                    const portugueseMessage = "Insira um e-mail válido";
+
+                    setMessage(isEnglishMessage ? portugueseMessage : extractedMessages[0]);
+                    console.log("TESTANDO", extractedMessages);
+                }
+            }
+        }
+    }, [payload]);
+
+    // Reset form messages
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage("");
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [message]);
 
     return (
         <div className={styles.form_container}>
@@ -35,24 +98,33 @@ const Register = () => {
             <p>
                 Crie uma conta e comece a compartilhar coisas do seu interesse agora mesmo
             </p>
+            {message && (
+                <FlashMessage 
+                    type={error ? "error" : "success"}
+                    message={message}
+                />
+            )}
             <form onSubmit={handleSubmit}>
-                <div className={styles.email}>
-                    <FaEnvelope />
-                    <input 
-                        type="email" 
-                        name="email" 
-                        placeholder="Seu E-mail" 
-                        onChange={(e) => setEmail(e.target.value)}
-                        autoFocus 
-                    />
-                </div>
-                <div className={styles.user}>
+            <div className={styles.user}>
                     <FaUser />
                     <input 
                         type="text" 
                         name="name" 
                         placeholder="Seu nome"
+                        value={name}
+                        ref={nameInputRef}
                         onChange={(e) => setName(e.target.value)}
+                        autoFocus
+                    />
+                </div>
+                <div className={styles.email}>
+                    <FaEnvelope />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Seu E-mail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
                     />
                 </div>
                 <div className={styles.password}>
@@ -61,6 +133,7 @@ const Register = () => {
                         type={isPassVisible ? "text" : "password"} 
                         name="password" 
                         placeholder="Senha"
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <button 
@@ -76,6 +149,7 @@ const Register = () => {
                         type={isConfirmPassVisible ? "text" : "password"} 
                         name="confirmPassword" 
                         placeholder="Confirmar senha"
+                        value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                     <button 
@@ -85,7 +159,8 @@ const Register = () => {
                         {isConfirmPassVisible ? <FaEyeSlash/> : <FaEye />}
                     </button>
                 </div>
-                <input type="submit" value="Cadastrar" />
+                {!loading && <input type="submit" value="Cadastrar" />}
+                {loading && <input type="submit" value="Aguarde..." disabled />}
             </form>
             <p className={styles.redirect}>
                 Já tem uma conta? <Link to="/login">Entre</Link>
