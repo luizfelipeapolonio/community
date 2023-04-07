d07dee377de24d55417aee076254a91084214be8
Luiz Felipe <luizfelipe0527@gmail.com>
2023-04-07 15:34:50 -0300

    Layout improvements

--- frontend/src/components/layout/DefaultUser.tsx
+++ frontend/src/components/layout/DefaultUser.tsx
@@ -4,13 +4,21 @@ import styles from "./DefaultUser.module.css";
 import { FaUser } from "react-icons/fa";
 
 interface Props {
+    size?: string;
+    fontSize?: string;
     position?: "center";
 }
 
-const DefaultUser = ({ position }: Props) => {
+const DefaultUser = ({ size, fontSize, position }: Props) => {
     return (
-        <div className={`${styles.default_container} ${position ? styles[position] : null}`}>
-            <FaUser />
+        <div 
+            className={`${styles.default_container} ${position ? styles[position] : null}`}
+            style={{
+                width: size,
+                height: size
+            }}
+        >
+            <FaUser style={{ fontSize }} />
         </div>
     );
 }
