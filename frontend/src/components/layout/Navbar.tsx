117c4adccd2d16c8b55b2fdda55dcf9b34d8d83c
Luiz Felipe <luizfelipe0527@gmail.com>
2023-04-13 21:40:23 -0300

    Responsive layout implemented

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -11,8 +11,10 @@ import {
     FaSignOutAlt, 
     FaSearch, 
     FaBookmark, 
-    FaInfoCircle 
+    FaInfoCircle,
+    FaHome 
 } from "react-icons/fa";
+import { BsFillFileRichtextFill, BsList, BsX } from "react-icons/bs";
 
 import { Link, NavLink } from "react-router-dom";
 
@@ -33,6 +35,14 @@ import { reset, logout } from "../../slices/authSlice";
 import { getCurrentUser } from "../../slices/userSlice";
 
 const Navbar = () => {
+    // Get window width
+    const getWindowWidth = (): number => {
+        const { innerWidth } = window;
+        return innerWidth;
+    }
+    
+    const [windowWidth, setWindowWidth] = useState<number>(getWindowWidth());
+
     const [user, setUser] = useState<IUser | null>(null);
     const [error, setError] = useState<boolean>(false);
     const [toggleMenu, setToggleMenu] = useState<string>("");
@@ -84,6 +94,17 @@ const Navbar = () => {
         dispatch(reset());
     }, [error]);
 
+    // Get the window width when it is resized
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(getWindowWidth());
+        }
+
+        window.addEventListener("resize", handleWindowResize)
+
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
     const handleLogout = async () => {
         await dispatch(logout());
         dispatch(reset());
@@ -130,6 +151,7 @@ const Navbar = () => {
                         <NavLink 
                             to="/"
                             className={({ isActive }) => isActive ? styles.active : ""}
+                            style={{ display: windowWidth < 580 ? "none" : "flex" }}
                         >
                             Home
                         </NavLink>
@@ -137,6 +159,7 @@ const Navbar = () => {
                             to="/post"
                             className={({ isActive }) => isActive ? styles.active : ""}
                             end
+                            style={{ display: windowWidth < 580 ? "none" : "flex" }}
                         >
                             Criar Post
                         </NavLink>
@@ -169,6 +192,24 @@ const Navbar = () => {
                                 </>
                             
                             </div>
+                            <Link 
+                                to="/" 
+                                className={styles.home} 
+                                style={{ display: windowWidth < 580 ? "flex" : "none" }} 
+                                onClick={closeDropdownMenu}
+                            >
+                                <div><FaHome /></div>
+                                <span>Home</span>
+                            </Link>
+                            <Link 
+                                to="/post" 
+                                className={styles.create_post} 
+                                style={{ display: windowWidth < 580 ? "flex" : "none" }} 
+                                onClick={closeDropdownMenu}
+                            >
+                                <div><BsFillFileRichtextFill /></div>
+                                <span>Criar Post</span>
+                            </Link>
                             <Link to={`/users/${user._id}`} className={styles.edit_profile} onClick={closeDropdownMenu}>
                                 <div><FaUserCircle /></div>
                                 <span>Meu Perfil</span>
@@ -191,18 +232,37 @@ const Navbar = () => {
                 </>
                 ) : (
                     <div className={styles.signup}>
-                        <NavLink 
-                            to="/login" 
-                            className={({ isActive }) => isActive ? styles.active : ""}
-                        >
-                            Entrar
-                        </NavLink>
-                        <NavLink 
-                            to="/register"
-                            className={({ isActive }) => isActive ? styles.active : ""}
-                        >
-                            Cadastrar
-                        </NavLink>
+                        <>
+                            {windowWidth > 490 ? (
+                            <>
+                                <NavLink 
+                                    to="/login" 
+                                    className={({ isActive }) => isActive ? styles.active : ""}
+                                >
+                                    Entrar
+                                </NavLink>
+                                <NavLink 
+                                    to="/register"
+                                    className={({ isActive }) => isActive ? styles.active : ""}
+                                >
+                                    Cadastrar
+                                </NavLink>
+                            </>):(
+                                <div className={styles.signupDrop}>
+                                    <button onClick={toggleDropdownMenu}>
+                                        {toggleMenu ? <BsX /> : <BsList />}
+                                    </button>
+                                    <div className={`${styles.drop_menu} ${styles[toggleMenu]}`}>
+                                        <NavLink to="/login" onClick={closeDropdownMenu}>
+                                            Entrar
+                                        </NavLink>
+                                        <NavLink to="/register" onClick={closeDropdownMenu}>
+                                            Cadastrar
+                                        </NavLink>
+                                    </div>
+                                </div>
+                            )}
+                        </>
                     </div>
                 )
             }
